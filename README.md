7ba1ff81df1f05f583e6ebb7079684ff8e064c26
Edit-Mr <elvismao.070512@gmail.com>
2024-02-25 10:29:21 +0800

    Add init website

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # SCAICT-Discord-Bot
+
+* main.py: Discord Bot
+* app.py: Flask Web App
+* generate-secrets.py: 生成 Secret Key
+
+## token.json
+
+```json
+{
+    "token":"",
+    "secret_key":"",
+    "client_id":"",
+    "client_secret":"",
+    "redirect_uri":"http://127.0.0.1:5000/callback"
+}
+```
---
+++ app.py
@@ -0,0 +1,56 @@
+from flask import Flask, redirect, request, session, url_for, render_template
+import requests
+import json
+
+app = Flask(__name__)
+
+# FILEPATH: /d:/GayHub/SCAICT-Discord-Bot/token.json
+with open("token.json") as file:
+    data = json.load(file)
+
+app.secret_key = data["secret_key"]
+client_id = data["client_id"]
+client_secret = data["client_secret"]
+redirect_uri = data["redirect_uri"]
+
+@app.route("/")
+def index():
+    return redirect(f"https://discord.com/api/oauth2/authorize?client_id={client_id}&redirect_uri={redirect_uri}&response_type=code&scope=identify")
+
+@app.route("/callback")
+def callback():
+    code = request.args.get("code")
+    data = {
+        "client_id": client_id,
+        "client_secret": client_secret,
+        "grant_type": "authorization_code",
+        "code": code,
+        "redirect_uri": redirect_uri,
+        "scope": "identify"
+    }
+    headers = {
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+    response = requests.post("https://discord.com/api/oauth2/token", data=data, headers=headers)
+    access_token = response.json()["access_token"]
+    headers = {
+        "Authorization": f"Bearer {access_token}"
+    }
+    user_response = requests.get("https://discord.com/api/users/@me", headers=headers)
+    user_data = user_response.json()
+    session["user"] = {
+        "name": user_data["username"],
+        "avatar": f"https://cdn.discordapp.com/avatars/{user_data['id']}/{user_data['avatar']}.png"
+    }
+    return redirect(url_for("profile"))
+
+@app.route("/profile")
+def profile():
+    user = session.get("user")
+    if user:
+        return render_template("home.html", username=user["name"], avatar=user["avatar"])
+    else:
+        return redirect(url_for("index"))
+
+if __name__ == "__main__":
+    app.run(debug=True)
