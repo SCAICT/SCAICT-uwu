cd346888ca627cc678fc6ea5857823e3f374b3cb
Each Chen <iach526526@gmail.com>
2024-03-02 12:04:20 +0800

    SQL syntax error

--- cog/core/SQL.py
+++ cog/core/SQL.py
@@ -6,15 +6,17 @@ def end(connection,cursor):#結束和SQL資料庫的會話
     connection.close()
     
 
-def write(userId, property, value):#欲更改的使用者,屬性,修改值
+def write(userId, property:str, value):#欲更改的使用者,屬性,修改值
     #建立連線
     connection=connect()
     cursor=connection.cursor()
     
     cursor.execute(f'SELECT `uid`,{property} FROM `USER` WHERE `uid`="{userId}"')
     RET=cursor.fetchall()
+    print("RET:",RET)
+    print(property,value)
     if (len(RET) !=0):#有 select 到東西，長度不為0
-        cursor.execute(f'UPDATE `USER` SET {str(property)}={value} WHERE `uid`={userId}')
+        cursor.execute(f'UPDATE `USER` SET {property}="{value}" WHERE `uid`={userId}')
         print(RET)
     else:
         print("找不到")#創造一份?
@@ -24,7 +26,7 @@ def read(userId, property):
     #建立連線
     connection=connect()
     cursor=connection.cursor()
-    cursor.execute(f'SELECT {property} FROM `USER` WHERE `uid`="{userId}"')
+    cursor.execute(f'SELECT {property} FROM `USER` WHERE `uid`={userId}')
     RET=cursor.fetchall()
     end(connection,cursor)
     return RET[0][0]
