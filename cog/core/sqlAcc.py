ad299eef8d5085c95cdf5c5e5ec3df3db5255b46
Each Chen <iach526526@gmail.com>
2024-03-02 18:44:31 +0800

    cloumn position and default value

--- cog/core/sqlAcc.py
+++ cog/core/sqlAcc.py
@@ -8,19 +8,19 @@ connection = mysql.connector.connect(host="",
 cursor=connection.cursor()
 
 cursor.execute("USE DCSQLtest ")
-cursor.execute("CREATE TABLE `USER`(uid BIGINT NOT NULL,\
-                                    point INT,\
-                                    ticket INT,\
-                                    charge_combo INT,\
-                                    next_lottery INT ,\
-                                    num_comment INT,\
-                                    last_charge DATETIME,\
-                                    last_comment DATETIME,\
+cursor.execute("CREATE TABLE `USER`(uid BIGINT NOT NULL DEFAULT '500',\
+                                    point INT NOT NULL DEFAULT 0,\
+                                    ticket INT NOT NULL DEFAULT 0,\
+                                    charge_combo INT NOT NULL DEFAULT 0,\
+                                    next_lottery INT NOT NULL DEFAULT 0,\
+                                    last_charge DATETIME NOT NULL DEFAULT 0,\
+                                    last_comment DATETIME NOT NULL DEFAULT '1970-01-01',\
+                                    today_comments INT NOT NULL DEFAULT '1970-01-01',\
                                     PRIMARY KEY (`uid`)\
                                     )"
               )
 cursor.execute("CREATE TABLE `CommentPoints`(seq INT AUTO_INCREMENT PRIMARY KEY,\
-                                              user_id BIGINT NOT NULL,\
+                                              user_id BIGINT NOT NULL DEFAULT '500',\
                                               times INT,\
                                               next_reward INT,\
                                               FOREIGN KEY (`user_id`) REFERENCES USER(`uid`) ON DELETE CASCADE\
