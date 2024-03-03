e314a9ae28228443fa132c831a14d7ba544c8c1e
Each Chen <iach526526@gmail.com>
2024-03-03 22:05:52 +0800

    default value

--- cog/core/sqlAcc.py
+++ cog/core/sqlAcc.py
@@ -1,28 +1,30 @@
 import mysql.connector
+
 #初始化資料庫程式，從創建表格開始
-connection = mysql.connector.connect(host="",
-                                      port="",
-                                      user="",  
-                                      passwd=""
+connection = mysql.connector.connect(host="172.233.85.95",
+                                    port="3306",
+                                    user="iach",  
+                                    passwd="Mew_mewCat@>",
+                                    database="DCSQLtest"
 )
 cursor=connection.cursor()
 
-cursor.execute("USE DCSQLtest ")
-cursor.execute("CREATE TABLE `USER`(uid BIGINT NOT NULL DEFAULT '500',\
+cursor.execute("USE DCSQLtest")
+cursor.execute("CREATE TABLE `USER`(uid BIGINT NOT NULL,\
                                     point INT NOT NULL DEFAULT 0,\
                                     ticket INT NOT NULL DEFAULT 0,\
                                     charge_combo INT NOT NULL DEFAULT 0,\
                                     next_lottery INT NOT NULL DEFAULT 0,\
-                                    last_charge DATETIME NOT NULL DEFAULT 0,\
-                                    last_comment DATETIME NOT NULL DEFAULT '1970-01-01',\
-                                    today_comments INT NOT NULL DEFAULT '1970-01-01',\
+                                    last_charge DATETIME NOT NULL DEFAULT '1970-01-01 00::00:00',\
+                                    last_comment DATE NOT NULL DEFAULT '1970-01-01',\
+                                    today_comments INT NOT NULL DEFAULT 0,\
                                     PRIMARY KEY (`uid`)\
                                     )"
               )
 cursor.execute("CREATE TABLE `CommentPoints`(seq INT AUTO_INCREMENT PRIMARY KEY,\
-                                              user_id BIGINT NOT NULL DEFAULT '500',\
-                                              times INT,\
-                                              next_reward INT,\
+                                              user_id BIGINT NOT NULL,\
+                                              times INT NOT NULL DEFAULT 2,\
+                                              next_reward INT NOT NULL DEFAULT 1,\
                                               FOREIGN KEY (`user_id`) REFERENCES USER(`uid`) ON DELETE CASCADE\
                                           )"
   )
