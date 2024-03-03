55537e5860c5f77fed75753935b7b33f72ade884
Each Chen <iach526526@gmail.com>
2024-03-03 22:07:18 +0800

    default vaalue & date caculate error

--- cog/comment.py
+++ cog/comment.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 import json
 from datetime import datetime
+from datetime import date
 from datetime import timedelta
 import csv
 from cog.core.SQL import read
@@ -13,16 +14,17 @@ def initUser(userId):#初始化(創建)傳入該ID的表格
     connection=connect()
     cursor=connection.cursor()
     
-    cursor.execute(f"INSERT INTO USER(uid,point,ticket,charge_combo,next_lottery,today_comments,last_charge,last_comment)\
-                                    VALUE({userId},0,0,0,0,0,'1970-01-01','1970-01-01')")
+    cursor.execute(f"INSERT INTO USER(uid) VALUE({userId})")#其他屬性在創造時MYSQL會給預設值
     end(connection,cursor)
-
+def getChannels():#要特殊用途頻道的列表，這裡會用來判斷是否在簽到頻簽到，否則不予授理
+    with open("./database/server.config.json", "r") as file:
+        return json.load(file)["SCAICT-alpha"]["channel"]
 def initCommentPoints(userId):#初始化(創建)傳入該ID的表格
     connection=connect()
     cursor=connection.cursor()
     
-    cursor.execute(f"INSERT INTO CommentPoints(uid,times,next_reward)\
-                                    VALUE({userId},0,0)")
+    cursor.execute(f"INSERT INTO CommentPoints(uid)\
+                                    VALUE({userId})")
     end(connection,cursor)
 class comment(commands.Cog):
 
@@ -49,7 +51,7 @@ class comment(commands.Cog):
         
         if today_comments==next_reward:
             point+=2
-            next_reward+=times**2#Each直覺:感覺怪怪的
+            next_reward+=times**2
             times+=1
             write(userId,"point",point)
             write(userId,"next_reward",next_reward,"CommentPoints")
@@ -63,21 +65,28 @@ class comment(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, message):
         userId=message.author.id
+        print(f"{userId}正在{message.channel.id}說話")
+        spChannel=getChannels()#特殊用途的channel
         #創建該user的資料表
+        if userId==1204265627016101969 or message.channel.id==spChannel["commandChannel"]:
+            #機器人會想給自己記錄電電點，必須排除
+            return
         if not(isExist(userId,"USER")):#該 uesr id 不在USER表格內，插入該筆用戶資料
             initUser(userId)
         if not(isExist(userId,"CommentPoints")):
-            initUser()
-        now = datetime.now().replace(microsecond=0)
+            initUser(userId)
+        now = date.today()
         delta = timedelta(days=1)
 
-        last_comment  = read(userId, 'last_charge')#SQL回傳型態:<class 'datetime.date'>
-        last_comment  = datetime.strptime(str(last_comment), '%Y-%m-%d %H:%M:%S')#strptime轉型後':<class 'datetime.datetime'>
+        last_comment  = read(userId, 'last_comment')#SQL回傳型態:<class 'datetime.date'>
+
 
         if(now-last_comment >= delta):#把昨天或更早的發言次數歸零
+            print("叮咚，今天第一次發言")
             self.reset(message, now)
 
         self.reward(userId, now)
+        
         
 
 def setup(bot):
