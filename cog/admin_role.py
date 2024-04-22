884bce1446c1cf19c058e63a5473d997a8a57c61
Each Chen <iach526526@gmail.com>
2024-04-22 22:28:52 +0800

    send btn to specific user

--- cog/admin_role.py
+++ cog/admin_role.py
@@ -26,7 +26,24 @@ class admin_role(build):
                 url="https://emojiisland.com/cdn/shop/products/Nerd_with_Glasses_Emoji_2a8485bc-f136-4156-9af6-297d8522d8d1_large.png?v=1571606036")
             embed.add_field(name="哈囉 點一下", value="  ", inline=False)
             await ctx.respond(embed=embed, view=self.roleView())
-
+            
+            
+    class Gift(discord.ui.View):
+        click : bool = None
+        def __init__(self):
+            super().__init__(timeout=None)  # timeout of the view must be set to None
+        @discord.ui.button(label="領取獎勵", style=discord.ButtonStyle.success)
+        async def hello(self, interaction: discord.Interaction, button: discord.ui.Button):
+            
+            await interaction.response.send_message("獎勵已發送", ephemeral=True)
+      
+    @discord.slash_command(name="發送禮物",description="dm_gift")
+    async def senddm(self, ctx,target,point):#dm gift
+        manager=ctx.author
+        target = await self.bot.fetch_user(target)#898141506588770334
+        view = self.Gift()
+        await target.send(view=view)
+        await ctx.respond(f"{manager} 發送 {point} 電電點給 {target}")
 
 def setup(bot):
     bot.add_cog(admin_role(bot))
