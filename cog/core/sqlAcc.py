27dac806f4721975f7d288cc949f5e09d7dd9017
Each Chen <iach526526@gmail.com>
2024-04-24 20:42:26 +0800

    new command

--- cog/core/sqlAcc.py
+++ cog/core/sqlAcc.py
@@ -3,7 +3,7 @@ from .SQL import linkSQL
 
 connection,cursor=linkSQL()
 
-cursor.execute("USE DCSQLtest")
+cursor.execute("USE DCSQLtest")#use your database
 cursor.execute("CREATE TABLE `USER`(uid BIGINT NOT NULL,\
                                     loveuwu TINYINT(1) NOT NULL DEFAULT 0 \
                                     point INT NOT NULL DEFAULT 0,\
@@ -23,18 +23,39 @@ cursor.execute("CREATE TABLE `CommentPoints`(seq INT AUTO_INCREMENT PRIMARY KEY,
                                               FOREIGN KEY (`uid`) REFERENCES USER(`uid`) ON DELETE CASCADE\
                                           )"
   )
-cursor.execute("CREATE TABLE `game` (seq BIGINT NOT NULL DEFAULT 0,\
-               niceColor VARCHAR(3) NOT NULL DEFAULT 'FFF',\
-               niceColorRound INT NOT NULL DEFAULT 0,\
-               lastID niceColorCount BIGINT DEFAULT 0);\
-                \
+cursor.execute("\
+               CREATE TABLE `game` (seq BIGINT NOT NULL DEFAULT 0,\
+                                    lastID BIGINT NOT NULL DEFAULT 0,\
+                                    niceColor VARCHAR(3) NOT NULL DEFAULT 'FFF',\
+                                    niceColorRound INT NOT NULL DEFAULT 0,\
+                                    niceColorCount BIGINT DEFAULT 0);\
                insert into game (seq) VALUES 0;\
                ")
 
-#查看所有databases
-# cursor.execute("SHOW DATABASES")
-# RET=cursor.fetchall()
-# print(RET)
+cursor.execute("\
+  USE CTF;\
+    CREATE TABLE `data`(id INT NOT NULL,\
+                         flags VARCHAR(255) NOT NULL,\
+                         score INT NOT NULL,\
+                         restrictions VARCHAR(255) NOT NULL,\
+                         message_id BIGINT NOT NULL,\
+                         case_status TINYINT(1) NOT NULL DEFAULT 0,\
+                         start_time DATETIME NOT NULL,\
+                         end_time DATETIME NOT NULL,\
+                          title VARCHAR(255) NOT NULL,\
+                          tried int NOT NULL DEFAULT 0,\
+                         PRIMARY KEY(`id`)\
+                         )\
+    CREATE TABLE `history`\
+    (data_id BIGINT ,\
+      uid BIGINT NOT NULL,\
+      count INT NOT NULL DEFAULT 0,\
+        soived TINYINT(1) NOT NULL DEFAULT 0)\
+      FOREIGN KEY (data_id) REFERENCES data(id)\
+    );\
+                  "
+    )
+
 print("done")
 cursor.close()
 connection.commit()
