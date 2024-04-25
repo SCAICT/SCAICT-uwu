7e5065970df963085e059060d7ef333a86baebf4
Winston Sung <winston21.sung@gmail.com>
2024-04-25 13:43:38 +0800

    Fix various lint issues - part 3
    - Duplicated renamed file sqlAcc.py => sql_acc.py
    - C0103: invalid-name
    - C0301: line-too-long
    - C0303: trailing-whitespace
    - C0325: superfluous-parens
    - C0411: wrong-import-order
    - R1705: no-else-return
    - R1719: simplifiable-if-expression
    - R1728: consider-using-generator
    - W0622: redefined-builtin
    - W1309: f-string-without-interpolation
    - W1514: unspecified-encoding
    
    Bug: #45
    Change-Id: Id4ec6866e3ae003c2e7b0320b6ffe8fb6b38508b

--- cog/core/sql_acc.py
+++ cog/core/sql_acc.py
@@ -5,9 +5,11 @@ from .sql import link_sql
 
 connection, cursor = link_sql()
 
+# Use your database instead
 cursor.execute("USE DCSQLtest")
-cursor.execute(
-    "CREATE TABLE `USER` (\
+
+cursor.execute("\
+    CREATE TABLE `USER`(\
         uid BIGINT NOT NULL,\
         loveuwu TINYINT(1) NOT NULL DEFAULT 0,\
         point INT NOT NULL DEFAULT 0,\
@@ -18,26 +20,56 @@ cursor.execute(
         last_comment DATE NOT NULL DEFAULT '1970-01-01',\
         today_comments INT NOT NULL DEFAULT 0,\
         PRIMARY KEY (`uid`)\
-    )"
-)
-cursor.execute(
-    "CREATE TABLE `CommentPoints` (\
+    )\
+")
+
+cursor.execute("\
+    CREATE TABLE `CommentPoints`(\
         seq INT AUTO_INCREMENT PRIMARY KEY,\
         uid BIGINT NOT NULL,\
         times INT NOT NULL DEFAULT 2,\
         next_reward INT NOT NULL DEFAULT 1,\
         FOREIGN KEY (`uid`) REFERENCES USER(`uid`) ON DELETE CASCADE\
-    )"
-)
-cursor.execute(
-    "CREATE TABLE `game` (\
-        seq BIGINT NOT NULL DEFAULT 0,\
-        lastID BIGINT DEFAULT 0\
-    );\
-    insert into game (seq) VALUES 0;"
-)
+    )\
+")
 
-# 查看所有databases
+cursor.execute("\
+    CREATE TABLE `game`(\
+        seq BIGINT NOT NULL DEFAULT 0,\
+        lastID BIGINT NOT NULL DEFAULT 0,\
+        niceColor VARCHAR(3) NOT NULL DEFAULT 'FFF',\
+        niceColorRound INT NOT NULL DEFAULT 0,\
+        niceColorCount BIGINT DEFAULT 0\
+    );\
+    insert into game (seq) VALUES 0;\
+")
+
+cursor.execute("\
+    USE CTF;\
+    CREATE TABLE `data`(\
+        id INT NOT NULL,\
+        flags VARCHAR(255) NOT NULL,\
+        score INT NOT NULL,\
+        restrictions VARCHAR(255) NOT NULL,\
+        message_id BIGINT NOT NULL,\
+        case_status TINYINT(1) NOT NULL DEFAULT 0,\
+        start_time DATETIME NOT NULL,\
+        end_time DATETIME NOT NULL,\
+        title VARCHAR(255) NOT NULL,\
+        tried int NOT NULL DEFAULT 0,\
+        PRIMARY KEY(`id`)\
+    )\
+    CREATE TABLE `history`(\
+        data_id BIGINT,\
+        uid BIGINT NOT NULL,\
+        count INT NOT NULL DEFAULT 0,\
+        soived TINYINT(1) NOT NULL DEFAULT 0\
+    )\
+    FOREIGN KEY (data_id) REFERENCES data(id)\
+    );\
+")
+
+# 查看所有database
 # cursor.execute("SHOW DATABASES")
 # ret = cursor.fetchall()
 # print(ret)
