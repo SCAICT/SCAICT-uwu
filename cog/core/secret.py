de2acb0acb9d27354eb56c81339fb0c704dfa25b
Each Chen <iach526526@gmail.com>
2024-07-18 22:47:31 +0800

    .env

--- cog/core/secret.py
+++ cog/core/secret.py
@@ -1,13 +1,21 @@
-# IMPORTANT!!!!Must ignore git!!!
 import mysql.connector
+import os
+from dotenv import load_dotenv
 
+load_dotenv(f'{os.getcwd()}/.env')
+
+
+HOSTIP = os.getenv('HOST')
+PORT= os.getenv('MYSQL_PORT')
+DBUSER = os.getenv('MYSQL_USER')
+DBNAME= os.getenv('MYSQL_DATABASE')
+PWD = os.getenv('MYSQL_PASSWORD')
 def connect():
-    connection = mysql.connector.connect(
-        host = "",
-        port = "",
-        user = "",
-        passwd = "",
-        database = "",
-        auth_plugin = "mysql_native_password"
+    connection = mysql.connector.connect(host=HOSTIP,
+                                        port=PORT,
+                                        user=DBUSER,  
+                                        passwd=PWD,
+                                        database=DBNAME
     )
+
     return connection
