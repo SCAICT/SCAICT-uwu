0f46bcb22a416a9ae1cccb48e0b74dc8c2d7ca89
Each Chen <iach526526@gmail.com>
2024-08-27 19:56:58 +0800

    fix: load_dotenv position

--- cog/core/secret.py
+++ cog/core/secret.py
@@ -6,6 +6,7 @@ from dotenv import load_dotenv
 import mysql.connector
 
 
+load_dotenv(f"{os.getcwd()}/.env")
 DB_USER = os.getenv("MYSQL_USER")
 DB_PASSWORD = os.getenv("MYSQL_PASSWORD")
 DB_NAME = os.getenv("MYSQL_DATABASE")
@@ -14,7 +15,6 @@ DB_PORT = os.getenv("MYSQL_PORT")
 
 
 def connect():
-    load_dotenv(f"{os.getcwd()}/.env")
     return mysql.connector.connect(
         user=DB_USER,
         password=DB_PASSWORD,
