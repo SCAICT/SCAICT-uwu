0403762b3c5ea8ae7aa220f6b1b448b51c891ade
Winston Sung <winston21.sung@gmail.com>
2024-10-08 00:12:52 +0800

    Packaging: Prepare src layout migration
    - language
    - system_message
    
    Change-Id: Ied7b8182e3564269c17c59aef78e2c9e2972ca76

---
+++ src/scaict_uwu/language/language_utils.py
@@ -0,0 +1,9 @@
+"""
+This is the module for the class for language utilities.
+"""
+
+
+class LanguageUtils:
+    """
+    The LanguageUtils class deals with language utilities.
+    """
--- src/scaict_uwu/system_message/message.py
+++ src/scaict_uwu/system_message/message.py
@@ -2,23 +2,42 @@
 This is the module for the class for all system messages.
 """
 
+# Local imports
+from ..language.language import Language
+
 
 class Message:
     """
     The Message class deals with fetching and processing of system messages.
     """
 
-    __use_lang: str = "zh-hant"
+    __use_lang: str | None = None
     """
-    __use_lang (str): The language tag of the language for the system message\
-        to use.
+    __use_lang (str|None): The language tag of the language for the system\
+        message to use.
     """
 
-    __in_lang: str = "zh-hant"
+    __in_lang: str | None = None
     """
-    __in_lang (str): The language tag of the language that the system message\
-        actually used.
+    __in_lang (str|None): The language tag of the language that the system\
+        message actually used.
     """
+
+    def __init__(
+        self,
+        key: str,
+        params: list,
+        use_lang: Language | None,
+    ) -> None:
+        """
+        Set the language tag of the language that the message expected to use.
+
+        Parameters:
+            key (str): Message key.
+            params (list): Message parameters.
+            use_lang: (Language|None): Language to use (None: defaults to\
+                current user language).
+        """
 
     def set_lang(self, lang_tag: str) -> None:
         """
@@ -40,4 +59,4 @@ class Message:
             str: Description of return value.
         """
 
-        return self.__use_lang or self.__in_lang
+        return self.__use_lang or self.__in_lang or ""
