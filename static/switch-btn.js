3dcc8266cef7ee3d456afdc77ded110137ba28ce
Winston Sung <winston21.sung@gmail.com>
2025-01-16 22:15:34 +0800

    Remove deprecated redundant code in switch-btn.js
    Change-Id: I6bb5dd1cc73186f5f2c056fd21b13608ae408822

--- static/switch-btn.js
+++ static/switch-btn.js
@@ -4,49 +4,56 @@ var switchButton = document.querySelector('.switch-button');
 var switchBtnRight = document.querySelector('.switch-button-case.right');
 var switchBtnLeft = document.querySelector('.switch-button-case.left');
 var activeSwitch = document.querySelector('.active');
-var numDraws = document.getElementById('numDraws');//決定要抽幾抽的表單
 
-//抓轉蛋機外觀元素，用來調整轉蛋機的外觀
+// 決定要抽幾抽的表單
+var numDraws = document.getElementById('numDraws');
+
+// 抓轉蛋機外觀元素，用來調整轉蛋機的外觀
 var st3 = document.getElementsByClassName('st3');
 var st4 = document.getElementsByClassName('st4');
 var st5 = document.getElementsByClassName('st5');
 var st6 = document.getElementsByClassName('st6');
-function changeMachine(color3,color4,color5,color6){
+
+function changeMachine(color3, color4, color5, color6) {
 	for (var i = 0; i < st3.length; i++) {
 		st3[i].style.fill = color3;
 	}
+
 	for (var i = 0; i < st4.length; i++) {
 		st4[i].style.fill = color4;
 	}
+
 	for (var i = 0; i < st5.length; i++) {
 		st5[i].style.fill = color5;
 	}
+
 	for (var i = 0; i < st6.length; i++) {
 		st6[i].style.fill = color6;
 	}
 }
-function switchLeft(){//左邊，單抽
+
+// 左邊，單抽
+function switchLeft() {
 	switchBtnRight.classList.remove('active-case');
 	switchBtnLeft.classList.add('active-case');
-	activeSwitch.style.left= '0%';
-	numDraws.value=1;
+	activeSwitch.style.left = '0%';
+	numDraws.value = 1;
 	changeMachine('#1e90ff', '#00bfff', '#87cefa', '#4682b4');
 }
 
-function switchRight(){//右邊，10連
+// 右邊，10連
+function switchRight() {
 	switchBtnRight.classList.add('active-case');
 	switchBtnLeft.classList.remove('active-case');
 	activeSwitch.style.left = '50%';
-	numDraws.value=10;
+	numDraws.value = 10;
 	changeMachine('#FF0000', '#FF4500', '#B22222', '#8B0000');
-	achine('#FFD700', '#FFA500', '#FF6347', '#d76a1d');// Suspected Redundant Code
 }
 
-switchBtnLeft.addEventListener('click', function(){
+switchBtnLeft.addEventListener('click', function() {
 	switchLeft();
 }, false);
 
-switchBtnRight.addEventListener('click', function(){
+switchBtnRight.addEventListener('click', function() {
 	switchRight();
 }, false);
-
