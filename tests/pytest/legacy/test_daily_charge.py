78382c4ba98f85fd3b16c2286274adb57291a828
Winston Sung <winston21.sung@gmail.com>
2026-07-25 15:41:21 +0800

    chore: Manual sync from REL0_1 v0.1.x - tests directory
    Change-Id: I2f3d59e06fd5b72046aea1898de9fba482a95bf1

--- tests/pytest/legacy/test_daily_charge.py
+++ tests/pytest/legacy/test_daily_charge.py
@@ -1,8 +1,13 @@
-import unittest
-from datetime import datetime
+# Future statements
+from __future__ import annotations
 
-from cog.daily_charge import Charge
-from cog.core.sql_abstract import UserRecord
+# Standard imports
+import datetime
+import unittest
+
+# Local imports
+import cog.core.sql_abstract
+import cog.daily_charge
 
 YUEVUWU = 1
 PANBOYU = 2
@@ -19,7 +24,7 @@ class TestIsForgivable(unittest.TestCase):
     #     }
     # ]
 
-    def test_is_forgivable(self):
+    def test_is_forgivable(self) -> None:
         test_cases_true = [
             "2025-03-13 00:00:00",
             "2025-03-13 23:59:59",
@@ -35,21 +40,22 @@ class TestIsForgivable(unittest.TestCase):
         for last_charge in test_cases_true:
             with self.subTest(last_charge=last_charge):
                 self.assertTrue(
-                    Charge.is_forgivable(
-                        datetime.strptime(last_charge, "%Y-%m-%d %H:%M:%S")
+                    cog.daily_charge.Charge.is_forgivable(
+                        datetime.datetime.strptime(last_charge, "%Y-%m-%d %H:%M:%S")
                     )
                 )
+
         for last_charge in test_cases_false:
             with self.subTest(last_charge=last_charge):
                 self.assertFalse(
-                    Charge.is_forgivable(
-                        datetime.strptime(last_charge, "%Y-%m-%d %H:%M:%S")
+                    cog.daily_charge.Charge.is_forgivable(
+                        datetime.datetime.strptime(last_charge, "%Y-%m-%d %H:%M:%S")
                     )
                 )
 
 
 class TestIsCrossDay(unittest.TestCase):
-    def test_cross_day(self):
+    def test_cross_day(self) -> None:
         test_cases_true = [
             ("2024-01-01 01:00:00", "2024-01-03 00:00:00"),
             ("2024-01-01 01:00:00", "2024-01-03 00:59:59"),
@@ -64,17 +70,18 @@ class TestIsCrossDay(unittest.TestCase):
         for last_charge, executed_at in test_cases_true:
             with self.subTest(last_charge=last_charge, executed_at=executed_at):
                 self.assertTrue(
-                    Charge.is_cross_day(
-                        datetime.strptime(last_charge, "%Y-%m-%d %H:%M:%S"),
-                        datetime.strptime(executed_at, "%Y-%m-%d %H:%M:%S"),
+                    cog.daily_charge.Charge.is_cross_day(
+                        datetime.datetime.strptime(last_charge, "%Y-%m-%d %H:%M:%S"),
+                        datetime.datetime.strptime(executed_at, "%Y-%m-%d %H:%M:%S"),
                     )
                 )
+
         for last_charge, executed_at in test_cases_false:
             with self.subTest(last_charge=last_charge, executed_at=executed_at):
                 self.assertFalse(
-                    Charge.is_cross_day(
-                        datetime.strptime(last_charge, "%Y-%m-%d %H:%M:%S"),
-                        datetime.strptime(executed_at, "%Y-%m-%d %H:%M:%S"),
+                    cog.daily_charge.Charge.is_cross_day(
+                        datetime.datetime.strptime(last_charge, "%Y-%m-%d %H:%M:%S"),
+                        datetime.datetime.strptime(executed_at, "%Y-%m-%d %H:%M:%S"),
                     )
                 )
 
@@ -82,77 +89,78 @@ class TestIsCrossDay(unittest.TestCase):
 class TestDailyChargeIntegrationTest(unittest.TestCase):
     """
     There will be three Variable:
+
     ```
     is_forgivable: bool # sign before downtime and not loss combo
     ```
     """
 
-    def test_panboyu3980(self):
-        last_charge = datetime(2025, 3, 13, 9, 5, 0)
-        is_forgivable = Charge.is_forgivable(last_charge)
+    def test_panboyu3980(self) -> None:
+        last_charge = datetime.datetime(2025, 3, 13, 9, 5, 0)
+        is_forgivable = cog.daily_charge.Charge.is_forgivable(last_charge)
         self.assertTrue(is_forgivable)
 
-        delta = Charge.reward(
+        delta = cog.daily_charge.Charge.reward(
             user_or_uid=PANBOYU,
             last_charge=last_charge,
-            executed_at=datetime(2025, 4, 12, 17, 26, 0),
+            executed_at=datetime.datetime(2025, 4, 12, 17, 26, 0),
             orig_combo=11,
             orig_point=1445,
             orig_ticket=4,
             is_forgivable=is_forgivable,
             testing=True,
         )
-        expected = UserRecord(
+        expected = cog.core.sql_abstract.UserRecord(
             uid=PANBOYU,
             charge_combo=12,
             point=1450,
-            last_charge=datetime(2025, 4, 12, 17, 26, 0),
+            last_charge=datetime.datetime(2025, 4, 12, 17, 26, 0),
         )
         self.assertEqual(delta, expected)
 
-    def test_w4lnu7__(self):
-        last_charge = datetime(2025, 3, 12, 21, 15, 0)
-        is_forgivable = Charge.is_forgivable(last_charge)
+    def test_w4lnu7__(self) -> None:
+        last_charge = datetime.datetime(2025, 3, 12, 21, 15, 0)
+        is_forgivable = cog.daily_charge.Charge.is_forgivable(last_charge)
         self.assertFalse(is_forgivable)
 
-        delta = Charge.reward(
+        delta = cog.daily_charge.Charge.reward(
             user_or_uid=WALNUT,
             last_charge=last_charge,
-            executed_at=datetime(2025, 4, 12, 20, 56, 0),
+            executed_at=datetime.datetime(2025, 4, 12, 20, 56, 0),
             orig_combo=24,
             orig_point=2564,
             orig_ticket=4,
             is_forgivable=is_forgivable,
             testing=True,
         )
-        expected = UserRecord(
+        expected = cog.core.sql_abstract.UserRecord(
             uid=WALNUT,
             charge_combo=1,
             point=2569,
-            last_charge=datetime(2025, 4, 12, 20, 56, 0),
+            last_charge=datetime.datetime(2025, 4, 12, 20, 56, 0),
         )
         self.assertEqual(delta, expected)
 
-    def test_hatakutsu_yuevu(self):
-        last_charge = datetime(2025, 4, 12, 16, 27, 0)
-        is_forgivable = Charge.is_forgivable(last_charge)
+    def test_hatakutsu_yuevu(self) -> None:
+        last_charge = datetime.datetime(2025, 4, 12, 16, 27, 0)
+        is_forgivable = cog.daily_charge.Charge.is_forgivable(last_charge)
         self.assertFalse(is_forgivable)
 
-        delta = Charge.reward(
+        delta = cog.daily_charge.Charge.reward(
             user_or_uid=YUEVUWU,
             last_charge=last_charge,
-            executed_at=datetime(2025, 4, 13, 2, 47, 0),
+            executed_at=datetime.datetime(2025, 4, 13, 2, 47, 0),
             orig_combo=2,
             orig_point=2751 - 5,
             orig_ticket=8,
             is_forgivable=is_forgivable,
             testing=True,
         )
-        expected = UserRecord(
+        expected = cog.core.sql_abstract.UserRecord(
             uid=YUEVUWU,
             charge_combo=3,
             point=2751,
-            last_charge=datetime(2025, 4, 13, 2, 47, 0),
+            last_charge=datetime.datetime(2025, 4, 13, 2, 47, 0),
         )
         self.assertEqual(delta, expected)
 
---
+++ tests/pytest/legacy/test_user_record.py
@@ -0,0 +1,34 @@
+# Future statements
+from __future__ import annotations
+
+# Standard imports
+import unittest
+
+# Third-party imports
+import mysql.connector.errors
+
+# Local imports
+import cog.core.sql
+import cog.core.sql_abstract
+
+YUEVUWU = 545234619729969152
+
+skip = False
+
+try:
+    with cog.core.sql.mysql_connection() as _:
+        pass
+except (RuntimeError, TypeError, mysql.connector.errors.Error):
+    skip = True
+
+
+class TestFromSQL(unittest.TestCase):
+    @unittest.skipIf(skip, "Failed to connect to database.")
+    def test_yuevuwu_exist(self) -> None:
+        data = cog.core.sql_abstract.UserRecord.from_sql(YUEVUWU)
+        self.assertIsNotNone(data)
+        print(data)
+
+
+if __name__ == "__main__":
+    unittest.main()
